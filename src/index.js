c59758692173a407d88b3c54203492440f998881
Cody Cogbill <codeman24@gmail.com>
2023-04-13 12:51:31 -0600

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  document
+    .getElementById("create-task-form")
+    .addEventListener("submit", (e) => {
+      e.preventDefault();
+
+      addTaskToForm(e.target["new-task-description"].value);
+    });
 });
+
+
+function addTaskToForm(newTask) {
+  const newLi = document.createElement("li") 
+  newLi.textContent = newTask;
+  document.getElementById("tasks").append(newLi);
+};
+
+
+
+
+
+
